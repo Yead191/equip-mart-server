7a299e65582f6142fe7e7823570e3d22e1ae5917
Yead <yead19@gmail.com>
2024-12-05 14:29:44 +0600

    added post and get operaiton

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express()
 const cors = require('cors');
@@ -8,9 +9,56 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get('/', (req, res)=>{
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wudpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+
+
+        const equipmentCollection = client.db("equipments").collection("equipment")
+
+
+        app.post('/equipments', async(req, res)=>{
+            const data = req.body
+            const result = await equipmentCollection.insertOne(data)
+            res.send(data)
+        })
+
+        app.get('/equipments', async(req, res)=>{
+            const result = await equipmentCollection.find().toArray()
+            res.send(result)
+        })
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('equi server on')
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('server running on ', port);
 })
